a01c32e597e07aedb5c6b5f583778c0569b07f98
KarolinaMungai <93986339+user-2048@users.noreply.github.com>
2023-11-24 16:01:20 +0000

    initialised PE in keypadInit()

--- EmbeddedSystemsProject/ProjectCode/main.c
+++ EmbeddedSystemsProject/ProjectCode/main.c
@@ -20,7 +20,6 @@
 #include "TExaS.h"
 #include "defines.h" // definitions of all ports in here!
 
-
 // 2. Declarations Section
 //   Global Variables
 //unsigned long In;  // input from PF4
@@ -33,31 +32,38 @@ char decodeKeyPress(unsigned char k); // This function returns the value of the 
 
 void Delay(void);
 
-
-
 // 3. Subroutines Section
 // MAIN: Mandatory for a C Program to be executable
 int main(void){    
 
 }
 
-// Subroutine to initialize port F pins for input and output
-// PF4 and PF0 are input SW1 and SW2 respectively
-// PF3,PF2,PF1 are outputs to the LED
-// Inputs: None
-// Outputs: None
-// Notes: These five pins are connected to hardware on the LaunchPad
-void keypadInit(){ volatile unsigned long delay; // PORTD and PORTE setup and number of rows/columns of the matrix.
+// PORTD and PORTE setup and number of rows/columns of the matrix.
+void keypadInit(){ volatile unsigned long delay; 
 	// init PORTD [0,3]
-	SYSCTL_RCGC2_R |= 0x00000020;     // 1) Clock - specific to PD
+	SYSCTL_RCGC2_R |= 0x08;     			// 1) activate bit 3
   delay = SYSCTL_RCGC2_R;           // delay  
-	GPIO_PORTD_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
-  GPIO_PORTD_CR_R = 0x3C;           // allow changes to PF4-0       
+	GPIO_PORTD_LOCK_R = 0x4C4F434B;   // 2) unlock PortD PD0  
+  GPIO_PORTD_CR_R = 0x3C;           // allow changes to PD3-0       
   GPIO_PORTD_AMSEL_R = 0x00;        // 3) disable analog function
   GPIO_PORTD_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
-  GPIO_PORTD_DIR_R = 0x0F;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTD_DIR_R = 0x0F;          // 5) PD0, PD1, PD2, PD3 output   
   GPIO_PORTD_AFSEL_R = 0x00;        // 6) no alternate function      
-  GPIO_PORTD_DEN_R = 0x0F;          // 7) enable digital pins PF4-PF0        
+  GPIO_PORTD_DEN_R = 0x0F;          // 7) enable digital pins PD4-PD0
+
+		// init PORTE [0,3]
+	SYSCTL_RCGC2_R |= 0x10;     			// 1) activate bit 4
+  delay = SYSCTL_RCGC2_R;           // delay  
+	GPIO_PORTE_LOCK_R = 0x4C4F434B;   // 2) unlock PortE PE0  
+  GPIO_PORTE_CR_R = 0x0f;           // allow changes to PE3-0       
+  GPIO_PORTE_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTE_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTE_DIR_R = 0x00;          // 5) PE0, PE1, PE2, PE3 inputs 
+  GPIO_PORTE_AFSEL_R = 0x00;        // 6) no alternate function      
+  GPIO_PORTE_DEN_R = 0x0F;          // 7) enable digital pins PE4-PE0
+
+	
+
 }
 
 
