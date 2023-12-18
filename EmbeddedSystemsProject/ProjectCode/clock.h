4cf701a742b2435136853dbf197e416ef827c3d9
KarolinaMungai <93986339+user-2048@users.noreply.github.com>
2023-12-18 13:05:45 +0000

    testing

--- EmbeddedSystemsProject/ProjectCode/clock.h
+++ EmbeddedSystemsProject/ProjectCode/clock.h
@@ -1,5 +1,16 @@
 // function declarations
 #include "defines.h"
+#include <stdio.h>
+
+//PLL related Defines
+#define SYSCTL_RIS_R          (*((volatile unsigned long *)0x400FE050))	
+#define SYSCTL_RCC_R          (*((volatile unsigned long *)0x400FE060))
+#define SYSCTL_RCC2_R         (*((volatile unsigned long *)0x400FE070))	
+
+//SysTick related Defines	
+#define NVIC_ST_CTRL_R        (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R      (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R     (*((volatile unsigned long *)0xE000E018))
 
 void SysTick_Init(void);
 
